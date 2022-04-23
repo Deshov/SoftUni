0c9fd1e716b7313988d6c34d086830c56d3fc7e1
Ivaylo Deshov <79753114+Deshov@users.noreply.github.com>
2022-04-23 09:14:28 +0300

    add exresice

---
+++ Programming Fundamentals/lesson 1 -  Syntax-Conditional-Statements-and-Loops/exercise/07. TriangleOfNumbers.js
@@ -0,0 +1,12 @@
+function foo(number){
+    let line=""
+    
+    for (let i = 1; i <=number; i++) {
+        for (let j = 1; j <= i; j++) {
+            line+=i+" "
+        }
+        console.log(line);
+        line=""
+    }
+    }
+ 
